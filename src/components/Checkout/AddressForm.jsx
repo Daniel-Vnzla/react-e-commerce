28434f56652f998fae40a9f0681025a57b507466
unknown <rune.escape888@gmail.com>
2020-12-11 18:27:29 -0400

    add: fetchShippingOptions and add go back to cart button

--- src/components/Checkout/AddressForm.jsx
+++ src/components/Checkout/AddressForm.jsx
@@ -17,6 +17,7 @@ const AddressForm = ({ checkoutToken }) => {
 	const formatSelectItems = (arr) => (
 		Object.entries(arr).map(([code, name]) => ({ id: code, label: name }))
 	)
+	const formattedShippingOptions = shippingOptions.map((option) => ({ id: option.id, label: `${option.description} - (${option.price.formatted_with_symbol})` }))
 
   const fetchShippingCountries = async (checkoutTokenId) => {
     const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
@@ -32,6 +33,13 @@ const AddressForm = ({ checkoutToken }) => {
     setShippingSubdivision(Object.keys(subdivisions)[0]);
   };
 
+  const fetchShippingOptions = async (checkoutTokenId, country, stateProvince = null) => {
+    const options = await commerce.checkout.getShippingOptions(checkoutTokenId, { country, region: stateProvince });
+
+    setShippingOptions(options);
+    setShippingOption(options[0].id);
+  };
+
 
   useEffect(() => {
   	fetchShippingCountries(checkoutToken.id);
@@ -40,16 +48,20 @@ const AddressForm = ({ checkoutToken }) => {
   useEffect(() => {
   	 if(shippingCountry) fetchSubdivisions(shippingCountry);
   },[shippingCountry])
+  
+  useEffect(() => {
+	  if (shippingSubdivision) fetchShippingOptions(checkoutToken.id, shippingCountry ,shippingSubdivision);
+  },[shippingSubdivision])
 
   return (
     <form className="form" >
 	    <h4>Ship Address</h4>
-			<CustomInput name="firstName" label="First Name"  type="text"/>
-			<CustomInput name="lastName" label="Last Name"  type="text"/>
-			<CustomInput name="address1" label="Address"  type="text"/>
-			<CustomInput name="email" label="Email"  type="email"/>
-			<CustomInput name="city" label="City"  type="text"/>
-			<CustomInput name="zip" label="ZIP / Postal code"  type="text"/>
+			<CustomInput name="firstName" label="First Name*"  type="text"/>
+			<CustomInput name="lastName" label="Last Name*"  type="text"/>
+			<CustomInput name="address1" label="Address*"  type="text"/>
+			<CustomInput name="email" label="Email*"  type="email"/>
+			<CustomInput name="city" label="City*"  type="text"/>
+			<CustomInput name="zip" label="ZIP / Postal code*"  type="text"/>
 
 			<div className="select-container">
 				<CustomSelect 
@@ -62,8 +74,16 @@ const AddressForm = ({ checkoutToken }) => {
 					name="ShippingSubdivision"
 					label="Shipping Subdivision" 
 					options={shippingSubdivisions} />
+					<CustomSelect 
+					onChange={({ target }) => setShippingOption(target.value)}
+					name="ShippingOption"
+					label="Shipping Option" 
+					options={formattedShippingOptions} />
 			</div>
+			<div className="form-actions">
+			  <Link to="/cart">Checkout</Link>			
 			  <button type="submit">Checkout</button>			
+			</div>
 		</form>
   )
 }
