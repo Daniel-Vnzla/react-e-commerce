7c67ec223e32bd4e0b34e7a12172b0e36832545d
unknown <rune.escape888@gmail.com>
2020-12-14 14:58:29 -0400

    add: PaymentForm component

--- src/components/Checkout/PaymentForm.jsx
+++ src/components/Checkout/PaymentForm.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import CustomInput from './CustomInput.jsx';
+import Review from './Review.jsx';
+import { Elements, CardElement, ElementsConsumer} from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
-const PaymentForm = (props) => {
+const stripePromise = loadStripe("...");
+
+const PaymentForm = ({ checkoutToken, backStep }) => {
   return (
-    <div>payment form</div>
+    <section>
+		<div className="payment">
+			<Review checkoutToken={checkoutToken} />
+			<div className="payment-method">
+				<h3>Payment method</h3>
+			</div>
+			<Elements stripe={stripePromise}>
+				<ElementsConsumer>
+					{({ elements, stripe }) => (
+						<form>
+							<CardElement />
+							<br />
+							<div className="actions">
+								<button onClick={backStep} >Back</button>
+								<button>Pay 2000$</button>
+							</div>
+						</form>
+					)}
+				</ElementsConsumer>
+			</Elements>
+			
+		</div>
+	</section>
   )
 }
 
